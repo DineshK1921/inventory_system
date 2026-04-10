c1998d8d0bc984012e20a81831c6b224d6ea44ca
Dinesh <kandharapudinesh@gmail.com>
2026-04-10 20:09:24 +0530

    Fixed 500 error - updated app.py and db.py

--- db.py
+++ db.py
@@ -1,10 +1,7 @@
 import os
-from dotenv import load_dotenv
 import psycopg
 
-load_dotenv()
-
-DATABASE_URL = os.getenv("DATABASE_URL")
+DATABASE_URL = os.environ.get("DATABASE_URL")
 
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL not set")
